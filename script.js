8f05d847a51ab01055d9174b9759b0075d6b6624
zainiba <47758673+zainiba@users.noreply.github.com>
2021-11-27 21:49:16 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function showBuyNow() {
 }
 
 //this calls the function above, 3000 milliseconds is 3 seconds, adjust here to make it a longer interval
-setTimeout("showBuyNow()", 5);
+setTimeout("showBuyNow()", 1);
 
-var str = "<h1 class=front1>My name is Desdemona</h1> <br><h3 class=front2>Intro blahbalblah</h3><br><br>",
+var str = "<h1 class=front1>My name is Desdemona</h1> <br><h3 class=front2>Nothing is going right these days. I forgot what I did yesterday. It's hard to visualize what I'm thinking because all I see are snakes. There's so much I want to do but for some reason I can't handle it all. If I don't get this done before sunset there'll be blood on the walls.</h3><br><br>",
     i = 0,
     isTag,
     text;
